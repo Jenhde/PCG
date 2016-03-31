28987e8d057bfa0288ed6a161c8e363ec600edf7
Jenhde <u201314834@upc.edu.pe>
2016-03-31 12:47:48 -0500

    V2

--- Main.gd
+++ Main.gd
@@ -5,6 +5,8 @@ onready var dim = Vector2(50,50)
 onready var perc=50
 onready var mat=[]
 onready var myseed = 20
+onready var currentScene = get_tree().get_root().get_child(get_tree().get_root().get_child_count() -1)
+
 
 func printmat():
 	for i in range(mat.size()):
@@ -28,10 +30,25 @@ func fillmat():
 			else:
 				matset(i,j,1)
 
+func draw():
+	var scene = ResourceLoader.load("res://tile.scn")
+	for i in range(dim.x):
+		for j in range(dim.y):
+			var tile = scene.instance()
+			tile.set_pos(Vector2(i*11,j*11))
+			add_child(tile)
+			
+	print(get_child_count())
+	
+	
+
+	
+	
 func _ready():
 	createMat()
 	fillmat()
-	printmat()
+	#printmat()
+	draw()
 
 
 
